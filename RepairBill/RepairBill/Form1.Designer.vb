3b8d52c867eef66c2eaa65f7d96dba68124ee8d1
Leigh Overcash <31711416+lovercash@users.noreply.github.com>
2018-09-26 13:15:09 -0400

    Final

---
+++ RepairBill/RepairBill/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtLabor = New System.Windows.Forms.TextBox()
+        Me.txtParts = New System.Windows.Forms.TextBox()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(22, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(140, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Hours of Labor"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(22, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cost of Parts"
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplay.Location = New System.Drawing.Point(437, 74)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(90, 65)
+        Me.btnDisplay.TabIndex = 3
+        Me.btnDisplay.Text = "&Display Bill"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(184, 48)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(343, 20)
+        Me.txtName.TabIndex = 4
+        '
+        'txtLabor
+        '
+        Me.txtLabor.Location = New System.Drawing.Point(184, 80)
+        Me.txtLabor.Name = "txtLabor"
+        Me.txtLabor.Size = New System.Drawing.Size(162, 20)
+        Me.txtLabor.TabIndex = 5
+        '
+        'txtParts
+        '
+        Me.txtParts.Location = New System.Drawing.Point(184, 113)
+        Me.txtParts.Name = "txtParts"
+        Me.txtParts.Size = New System.Drawing.Size(162, 20)
+        Me.txtParts.TabIndex = 6
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 25
+        Me.lstDisplay.Location = New System.Drawing.Point(27, 171)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(500, 204)
+        Me.lstDisplay.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(575, 422)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.txtParts)
+        Me.Controls.Add(Me.txtLabor)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Repair Bill"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtLabor As TextBox
+    Friend WithEvents txtParts As TextBox
+    Friend WithEvents lstDisplay As ListBox
+End Class
---
+++ RepairBill/RepairBill/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        'Clear Fields
+        lstDisplay.Items.Clear()
+
+        Dim custName As String
+        Dim labor, parts, total As Double
+
+        If IsNumeric(txtLabor.Text) And IsNumeric(txtParts.Text) And txtName.Text <> "" Then
+
+            labor = CDbl(txtLabor.Text)
+            parts = CDbl(txtParts.Text)
+            custName = txtName.Text
+
+            Dim laborCost As Double = labor * 35
+            Dim partsCost As Double = parts + (parts * 0.05)
+            total = laborCost + partsCost
+
+            lstDisplay.Items.Add("Customer: " & custName)
+            lstDisplay.Items.Add("Labor: " & FormatCurrency(laborCost))
+            lstDisplay.Items.Add("Parts: " & FormatCurrency(partsCost))
+            lstDisplay.Items.Add("Total Cost:" & FormatCurrency(total))
+
+        Else
+            MessageBox.Show("All fields required")
+
+
+        End If
+
+    End Sub
+End Class
